f83376bb79f2f76830f19856cc835a6c406a1c00
Spicer <spicer.bak.frf@gmail.com>
2023-10-06 11:43:23 -0400

    added try catch to make sure we don't crash on nmea strings with out good info

--- DataAcq/setSysClockFromGGA.py
+++ DataAcq/setSysClockFromGGA.py
@@ -11,12 +11,15 @@ def setClockfromGGA(serialport):
                        timeout=2, stopbits=serial.STOPBITS_ONE) as ser:
         while True:
             line = ser.readline().decode('ascii', errors='replace')  # first read the line
+            try:
+                if line.startswith("$GNGGA"):
+                    # set my pi clock to GGA string (assume correct day)
+                    timestr = datetime.datetime.strptime(line.split(',')[1], '%H%M%S.%f').time().strftime("%H:%M:%S")
+                    os.system(f"timedatectl set-time {timestr}")
+                    print(f'System Time Changed to {timestr}')
+            except:
+                continue
+            sys.exit()
 
-            if line.startswith("$GNGGA"):
-                # set my pi clock to GGA string (assume correct day)
-                timestr = datetime.datetime.strptime(line.split(',')[1], '%H%M%S.%f').time().strftime("%H:%M:%S")
-                os.system(f"timedatectl set-time {timestr}")
-                sys.exit()
-                git s
-if name == "__main__":
+if __name__ == "__main__":
     setClockfromGGA(serialPort)
