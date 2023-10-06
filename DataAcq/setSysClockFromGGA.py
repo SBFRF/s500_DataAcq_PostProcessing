d191fac63b07f9f5518d2e50288b6f0db21a33a8
Spicer <spicer.bak.frf@gmail.com>
2023-10-06 15:25:50 -0400

    fixed some logic and troubleshooting

--- DataAcq/setSysClockFromGGA.py
+++ DataAcq/setSysClockFromGGA.py
@@ -1,25 +1,47 @@
-"""this script when run will set the system clock from the NMEA GGA data streaming over the identified serial port"""
 import serial
 import datetime
 import os, sys
 from datetime import date
+import time
 
-serialPort = '/dev/ttyACM1'
 
-def setClockfromGGA(serialport):
+def setClockfromGGA(serialport, serialPort='/dev/ttyACM1', tryCount=100, sleepTimeMin=1):
+    """this function will set the system clock from the NMEA GGA data streaming over the identified serial port.
+
+    Args:
+        tryCount: how many times it will try (default =100)
+        sleepTimeMin: how long to wait between each try (default=1 min)
+        serialPort: a string describin the serialPort on the device over which to expect the GGA                strings
+
+    Returns:
+        None
+    """
+
+    # first turn off the auto time sync function (it has nothing to time sync to)
+    os.system("systemctl stop systemd-timesyncd.service")
     with serial.Serial(port=serialport, baudrate=115200, bytesize=8,
                        timeout=2, stopbits=serial.STOPBITS_ONE) as ser:
-        while True:
+
+       for i in range(tryCount):
             line = ser.readline().decode('ascii', errors='replace')  # first read the line
-            try:
-                if line.startswith("$GNGGA"):
-                    # set my pi clock to GGA string (assume correct day)
-                    timestr = datetime.datetime.strptime(line.split(',')[1], '%H%M%S.%f').time().strftime("%H:%M:%S")
+            if line.startswith("$GNGGA"):
+                try:
+                    #first parse
+                    timestr = datetime.datetime.strptime(line.split(',')[1], '%H%M%S.%f').time().strftime("%H:%M:%S.%f")
+                    # set my rasp-pi clock to GGA string (assume correct day)
                     os.system(f"timedatectl set-time {timestr}")
                     print(f'System Time Changed to {timestr}')
-            except:
-                continue
-            sys.exit()
+                    break
+
+                except:
+                    print(f'failed {line}')
+                    print(f'sleeping for {sleepTimeMin} minutes')
+                    time.sleep(sleepTimeMin*60)
+
+                    continue
+
+
+
 
 if __name__ == "__main__":
     setClockfromGGA(serialPort)
