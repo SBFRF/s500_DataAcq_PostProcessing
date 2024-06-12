954db625a98f20a121a684489848d8568e2db975
djason-todd <112493630+djason-todd@users.noreply.github.com>
2024-06-12 08:35:48 -0400

    Add files via upload
    Signed-off-by: djason-todd <112493630+djason-todd@users.noreply.github.com>

---
+++ DataAcq/setSysClockFromGGA.py
@@ -0,0 +1,25 @@
+"""this script when run will set the system clock from the NMEA GGA data streaming over the identified serial port"""
+import serial
+import datetime
+import os, sys
+from datetime import date
+
+serialPort = '/dev/ttyACM1'
+
+def setClockfromGGA(serialport):
+    with serial.Serial(port=serialport, baudrate=115200, bytesize=8,
+                       timeout=2, stopbits=serial.STOPBITS_ONE) as ser:
+        while True:
+            line = ser.readline().decode('ascii', errors='replace')  # first read the line
+            try:
+                if line.startswith("$GNGGA"):
+                    # set my pi clock to GGA string (assume correct day)
+                    timestr = datetime.datetime.strptime(line.split(',')[1], '%H%M%S.%f').time().strftime("%H:%M:%S")
+                    os.system(f"timedatectl set-time {timestr}")
+                    print(f'System Time Changed to {timestr}')
+            except:
+                continue
+            sys.exit()
+
+if __name__ == "__main__":
+    setClockfromGGA(serialPort)
